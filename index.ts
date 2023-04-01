b021f64811d68cf5726643d6d09f37115704fe54
Charles Cole <sismondi@Nicolette.local>
2023-04-01 19:14:23 +0200

    init

---
+++ index.ts
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const db = require('./queries.ts')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ queries.ts
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB_NAME,
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+})
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    getUsers,
+    getUserById,
+}
